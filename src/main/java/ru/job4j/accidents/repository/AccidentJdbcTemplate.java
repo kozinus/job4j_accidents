1095a027e7bd19b6f3342e9b82028d81dbe45235
kozinus <kkozin2403@gmail.com>
2024-07-26 05:03:28 +0300

    0. Spring DataSource [#6878 #504045]

--- src/main/java/ru/job4j/accidents/repository/AccidentJdbcTemplate.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentJdbcTemplate.java
@@ -3,11 +3,13 @@ package ru.job4j.accidents.repository;
 import lombok.AllArgsConstructor;
 import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.mapper.AccidentRowMapper;
 import ru.job4j.accidents.model.Accident;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.*;
 
 @Repository
@@ -17,15 +19,20 @@ public class AccidentJdbcTemplate {
     private final JdbcTemplate jdbc;
 
     public Optional<Accident> save(Accident accident) {
-        boolean flag = false;
-        flag = jdbc.update("insert into accidents (ac_name, ac_text, ac_address, type_id) values (?, ?, ?, ?)",
-                accident.getName(), accident.getText(), accident.getAddress(), accident.getType().getId()) != 0;
+        GeneratedKeyHolder holder = new GeneratedKeyHolder();
+        jdbc.update(con -> {
+            PreparedStatement statement = con.prepareStatement(
+                    "insert into accidents (ac_name, ac_text, ac_address, type_id) values (?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, accident.getName());
+            statement.setString(2, accident.getText());
+            statement.setString(3, accident.getAddress());
+            statement.setInt(4, accident.getType().getId());
+            return statement;
+        }, holder);
 
-        if (flag) {
-            SqlRowSet rowSet = jdbc.queryForRowSet("select max(id) from accidents");
-            rowSet.next();
-            accident.setId(rowSet.getInt("max"));
-        }
+        accident.setId((Integer) holder.getKeys().get("id"));
+
         return Optional.of(accident);
     }
 
@@ -45,9 +52,9 @@ public class AccidentJdbcTemplate {
         Map<Integer, Accident> accidentMap = new HashMap<>();
         jdbc.query(
                 "SELECT * FROM accidents AS ac "
-                        + "LEFT JOIN accident_types AS at ON ac.type_id = at.id "
-                        + "LEFT JOIN participates AS p ON ac.id = p.accident_id "
-                        + "LEFT JOIN accident_rules AS r ON p.rule_id = r.id "
+                        + "JOIN accident_types AS at ON ac.type_id = at.id "
+                        + "JOIN participates AS p ON ac.id = p.accident_id "
+                        + "JOIN accident_rules AS r ON p.rule_id = r.id "
                         + "WHERE ac.id = "
                         + id,
                 new AccidentRowMapper(accidentMap)
