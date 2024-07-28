57b2a0276865d0a5ba430d8392f8607b25654654
kozinus <kkozin2403@gmail.com>
2024-07-28 17:12:53 +0300

    1. Авторизация через JDBC [#2094]

--- db/scripts/009_dml_insert_users.sql
+++
@@ -1,2 +0,0 @@
-insert into users(username, password) values('user', '123456');
-insert into authorities(username, authority) values('user', 'ADMIN');
--- src/main/java/ru/job4j/accidents/SecurityConfig.java
+++ src/main/java/ru/job4j/accidents/SecurityConfig.java
@@ -3,9 +3,11 @@ package ru.job4j.accidents;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,6 +18,15 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     DataSource ds;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(ds)
+                .withUser(User.withUsername("user")
+                        .password(passwordEncoder().encode("123456"))
+                        .roles("USER"));
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
