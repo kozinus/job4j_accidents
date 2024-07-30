15c0da32bdfd6c1dd34bc92efa0192cda08812c0
kozinus <kkozin2403@gmail.com>
2024-07-30 12:26:54 +0300

    2. Регистрация пользователя [#296069]

--- src/main/java/ru/job4j/accidents/SecurityConfig.java
+++ src/main/java/ru/job4j/accidents/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login", "/reg")
                 .permitAll()
                 .antMatchers("/**")
-                .hasAnyRole("ROLE_ADMIN", "ROLE_USER")
+                .hasAnyRole("ADMIN", "USER")
                 .and()
                 .formLogin()
                 .loginPage("/login")
